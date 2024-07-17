1ea4e021c28ea23582d6caf427a12bbef195e538
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-07-17 11:35:10 +0100

    done

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// Set up Multer for file uploading
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
+// Endpoint to handle file upload and return metadata
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
